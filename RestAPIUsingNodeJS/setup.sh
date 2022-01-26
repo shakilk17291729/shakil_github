566ac653bd67135a0791fa6274268184b78fe04b
shakil <shakilk1729@gmail.com>
2022-01-26 01:27:13 -0800

    Changed port for message

--- RestAPIUsingNodeJS/setup.sh
+++ RestAPIUsingNodeJS/setup.sh
@@ -9,4 +9,4 @@ npm install express
 
 
 echo "To run the node server you can do \"nohup node ./server.js &\""
-echo "From the client browser machine do http://localhost:8080/getUsers "
+echo "From the client browser machine do http://localhost:4000/getUsers "
